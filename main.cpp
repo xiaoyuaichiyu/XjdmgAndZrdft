b40895598cb1ffbc0a4efbbf54be328284289612
xiaoyuaichiyu <chitaiyuchitaiyu@gmail.com>
2021-01-17 22:03:01 +0800

    d

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "include/perceptron.h"
+#include "include/base.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    Perceptron  kkk;
+    kkk.PerceptronRun("Mnist/mnist_train.csv","Mnist/mnist_test.csv",50);
+
+    MatrixXd m(2, 2);
+    m<<1,2,3,4;
+  
+    std::cout << m << std::endl;
+}
